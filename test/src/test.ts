ffb941441eba4f8f3951be8cb57e70f0fbecbf6c
ShunKUN1182 <2250378@ecc.ac.jp>
2025-12-29 04:20:18 +0900

    整理：comment

--- test/src/test.ts
+++ test/src/test.ts
@@ -1,33 +1,33 @@
 export const test = () => {
     // console.log("test");
-    // const name: string = "mike"; //string
-    // const age: number = 25; //number
-    // const isStudent: boolean = false; //boolean
-    // const hobbies: string[] = ["sports", "music", "game"]; //array
-    // const addres: object = {
-    //     street: "123 main st",
-    //     city: "osaka",
-    //     state: "CA",
-    // }; //object
-    // const today: Date = new Date(); //date
-    // const none: null = null; //null
-    // const notDefined: undefined = undefined; //undefined
-    // const randum: any = "Hello Would"; //any
-    // const genber: "male" | "female" = "male"; //ユニオン型
-    // const value: string | number | null = "foo"; //これもユニオン型
-    // console.log(
-    //     name,
-    //     age,
-    //     isStudent,
-    //     hobbies,
-    //     addres,
-    //     today,
-    //     none,
-    //     notDefined,
-    //     randum,
-    //     genber,
-    //     value
-    // );
+    const name: string = "mike"; //string
+    const age: number = 25; //number
+    const isStudent: boolean = false; //boolean
+    const hobbies: string[] = ["sports", "music", "game"]; //array
+    const addres: object = {
+        street: "123 main st",
+        city: "osaka",
+        state: "CA",
+    }; //object
+    const today: Date = new Date(); //date
+    const none: null = null; //null
+    const notDefined: undefined = undefined; //undefined
+    const randum: any = "Hello Would"; //any
+    const genber: "male" | "female" = "male"; //ユニオン型
+    const value: string | number | null = "foo"; //これもユニオン型
+    console.log(
+        name,
+        age,
+        isStudent,
+        hobbies,
+        addres,
+        today,
+        none,
+        notDefined,
+        randum,
+        genber,
+        value
+    );
     // const reverseArray = (arr: string[], hasTom?: boolean): string[] => {
     //     const reverseArray: string[] = [];
     //     let index: number = arr.length - 1;
@@ -61,6 +61,7 @@ export const test = () => {
     // interface AdminUser extends UserProps {
     //     role: string; //アドミンやリーダーなどを入れれる
     // }
+
     type UserProps = {
         userId: number;
         name: string;
