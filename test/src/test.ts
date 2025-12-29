9b9b9bcd06282d995424757fd5e9075a9dc5e317
ShunKUN1182 <2250378@ecc.ac.jp>
2025-12-29 18:43:08 +0900

    履修終了

--- test/src/test.ts
+++ test/src/test.ts
@@ -1,5 +1,5 @@
 export const test = () => {
-    // console.log("test");
+    console.log("test");
     const name: string = "mike"; //string
     const age: number = 25; //number
     const isStudent: boolean = false; //boolean
@@ -28,40 +28,36 @@ export const test = () => {
         genber,
         value
     );
-    // const reverseArray = (arr: string[], hasTom?: boolean): string[] => {
-    //     const reverseArray: string[] = [];
-    //     let index: number = arr.length - 1;
-    //     while (index >= 0) {
-    //         reverseArray.push(arr[index]);
-    //         index--;
-    //     }
-    //     if (hasTom) {
-    //         reverseArray.push("tom");
-    //     }
-    //     return reverseArray;
-    // };
-    // console.log(reverseArray(["a", "b", "c"], true));
-
-    // const user = {
-    //     userId: 1,
-    //     name: "mike",
-    //     age: 18,
-    //     email: "atekusi1234@gmail.com",
-    //     isActive: true,
-    // };
-
-    // interface UserProps {
-    //     userId: number;
-    //     name: string;
-    //     age: number;
-    //     email: string;
-    //     isActive: boolean;
-    // }
-
-    // interface AdminUser extends UserProps {
-    //     role: string; //アドミンやリーダーなどを入れれる
-    // }
-
+    const reverseArray = (arr: string[], hasTom?: boolean): string[] => {
+        const reverseArray: string[] = [];
+        let index: number = arr.length - 1;
+        while (index >= 0) {
+            reverseArray.push(arr[index]);
+            index--;
+        }
+        if (hasTom) {
+            reverseArray.push("tom");
+        }
+        return reverseArray;
+    };
+    console.log(reverseArray(["a", "b", "c"], true));
+    const user = {
+        userId: 1,
+        name: "mike",
+        age: 18,
+        email: "atekusi1234@gmail.com",
+        isActive: true,
+    };
+    interface UserProps {
+        userId: number;
+        name: string;
+        age: number;
+        email: string;
+        isActive: boolean;
+    }
+    interface AdminUser extends UserProps {
+        role: string; //アドミンやリーダーなどを入れれる
+    }
     type UserProps = {
         userId: number;
         name: string;
@@ -72,10 +68,12 @@ export const test = () => {
     type AdminUser = UserProps & {
         role: string;
     };
-
-    const createAccount = (accountInfo: AdminUser) => {};
-    const createUserAccount = (accountInfo: UserProps) => {};
-
+    const createAccount = (accountInfo: AdminUser) => {
+        accountInfo;
+    };
+    const createUserAccount = (accountInfo: UserProps) => {
+        accountInfo;
+    };
     createUserAccount({
         userId: 1,
         name: "mike",
@@ -91,4 +89,51 @@ export const test = () => {
         isActive: true,
         role: "atekusi",
     });
+    class User {
+        userId: number;
+        name: string;
+        age: number;
+        email: string;
+        isActive: boolean;
+        constructor(userId: number, name: string, age: number, email: string, isActive: boolean) {
+            this.userId = userId;
+            this.name = name;
+            this.age = age;
+            this.email = email;
+            this.isActive = isActive;
+        }
+        protected getProfile(): string {
+            return `Name: ${this.name},Age: ${this.age}`;
+        }
+        static sayHelloWould(): void {
+            console.log("hello would");
+        }
+    }
+    const user = new User(1, "mike", 25, "atekusi", true);
+    //public, private, protected
+    const foo = (arg: string): string => {
+        return arg;
+    };
+    const foo2 = <T>(arg: T): T => {
+        return arg;
+    };
+      foo2<string>("hey");
+      foo2<number>(29);
+    foo2<boolean>(true);
+    const fetchUserData = async () => {
+      const response = await fetch("userId/data/status", {
+        userId: 1;
+      })
+      return response.data.status;
+  }
+  const isActive:boolean = fetchUserData() ?? false;
+  
+    const fetchUserData = async () => {
+      const response = await fetch("userId/data/status", {
+        userId: 1;
+      })
+      return response.data;
+    }
+  const userInfoData = fetchUserData();
+  const feachName = userInfoData?.family?.father;
 };
